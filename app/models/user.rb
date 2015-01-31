48d968b4418b2cf769bb4d82188e93e14915cc11
Matthew Moore <m3moore@gmail.com>
2015-01-31 14:52:06 -0800

    parse names right

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ActiveRecord::Base
       existing_user = User.create(
         linkedinid:        omniauth_auth['uid'],
         email:           omniauth_auth['info']['email'],
-        first_name:      omniauth_auth['info']['name'].rpartition(" ").first.rpartition(" ").first,
-        middle_name:     omniauth_auth['info']['name'].rpartition(" ").first.rpartition(" ").last,
+        first_name:      omniauth_auth['info']['name'].partition(" ").first.partition(" ").first,
+        middle_name:     omniauth_auth['info']['name'].partition(" ").first.partition(" ").last,
         last_name:       omniauth_auth['info']['name'].rpartition(" ").last
       )
     end
