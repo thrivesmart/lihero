23651885ae14e40b3cce693bcde43d6aa0fc71ce
Matthew Moore <m3moore@gmail.com>
2015-03-09 19:33:38 -0700

    keep trying linkedin

--- lib/s3_cache.rb
+++ lib/s3_cache.rb
@@ -1,29 +1,22 @@
 module S3Cache
 
-  def self.set_credentials
+  def self.fetch_credentials
     # The following should be done automatically by the gem, according to the documentation 
-    # Aws.config(
-    #   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    #   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    #   region: ENV['AWS_REGION']
-    # )
+    creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
   end
 
   def self.fetch(cache_key, bucket_name, expires_in = 14.days)
-    set_credentials
-    
     begin
-      return Aws::S3::Client.new.get_object(bucket: bucket_name, key: cache_key).body.read
-    rescue # Aws::S3::Errors::NoSuchKey
+      return Aws::S3::Client.new(credentials: fetch_credentials).get_object(bucket: bucket_name, key: cache_key).body.read
+    rescue Aws::S3::Errors::NoSuchKey
        return nil
     end
   end
   
-  def self.write(cache_key, value, bucket_name, expires_in = 14.days) 
-    set_credentials
-    
-    bucket = Aws::S3::Resource.new.bucket(bucket_name)
-    bucket.put_object(key: cache_key, body: value, acl: :public_read)
+  def self.write(cache_key, value, bucket_name, expires_in = 14.days)
+    hash = {bucket: bucket_name, key: cache_key, body: StringIO.new(value), acl: :public_read}
+    puts hash.inspect
+    Aws::S3::Client.new(credentials: fetch_credentials).put_object(bucket: bucket_name, key: cache_key, body: StringIO.new(value), acl: :public_read)
   end
   
 end
